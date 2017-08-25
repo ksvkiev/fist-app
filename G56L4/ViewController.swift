6012beb7cbebc3a10987545b6fd0b4b54f933125
ksvkiev <sergejkosovan@Air.local>
2017-08-25 17:40:36 +0300

    Initial Commit

---
+++ G56L4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  G56L4
+//
+//  Created by Сергей Косован on 25.08.17.
+//  Copyright © 2017 Сергей Косован. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
